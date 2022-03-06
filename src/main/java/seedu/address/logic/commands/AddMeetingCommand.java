e286e26e32cf1975400882b146ac6a8e32892fbf
raynerljm <raynerljm@gmail.com>
2022-03-06 22:02:58 +0800

    Add AddMeetingCommand class

---
+++ src/main/java/seedu/address/logic/commands/AddMeetingCommand.java
@@ -0,0 +1,42 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+import seedu.address.model.meeting.Meeting;
+
+/**
+ * Adds a meeting to the address book.
+ */
+public class AddMeetingCommand extends Command {
+
+    public static final String COMMAND_WORD = "addMeeting";
+    public static final String MESSAGE_SUCCESS = "New person added: %1$s";
+    public static final String MESSAGE_OVERLAPPING_MEETING = "There is an existing"
+           + " meeting overlapping with this meeting.";
+
+    private final Meeting toAdd;
+
+    /**
+     * Creates an AddMeetingCommand to add the specified {@code Meeting}
+     */
+    public AddMeetingCommand(Meeting meeting) {
+        requireNonNull(meeting);
+        toAdd = meeting;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        model.addMeeting(toAdd);
+        return new CommandResult(String.format(MESSAGE_SUCCESS, toAdd));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof AddMeetingCommand // instanceof handles nulls
+                && toAdd.equals(((AddMeetingCommand) other).toAdd));
+    }
+}
