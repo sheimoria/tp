4a99131584a40deac50199f6d8e3c36312efad39
myc37 <nvjn37@gmail.com>
2022-03-06 23:03:58 +0800

    Add Policy class

---
+++ src/main/java/seedu/address/model/person/policy/Policy.java
@@ -0,0 +1,85 @@
+package seedu.address.model.person.policy;
+
+import java.util.Objects;
+
+import seedu.address.model.person.Name;
+
+/**
+ * Represents a Policy in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Policy {
+
+    // Identity fields
+    private final Name name;
+    private final Name company;
+    private final Name policyManager;
+    private final Premium premium;
+
+    /**
+     * Every field must be present and not null.
+     */
+    public Policy(Name name, Name company, Name policyManager, Premium premium) {
+        this.name = name;
+        this.company = company;
+        this.policyManager = policyManager;
+        this.premium = premium;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public Name getCompany() {
+        return company;
+    }
+
+    public Name getPolicyManager() {
+        return policyManager;
+    }
+
+    public Premium getPremium() {
+        return premium;
+    }
+
+    /**
+     * Returns true if both Policies have the same identity fields.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        if (!(other instanceof Policy)) {
+            return false;
+        }
+
+        Policy otherPolicy = (Policy) other;
+        return otherPolicy.getName().equals(getName())
+                && otherPolicy.getCompany().equals(getCompany())
+                && otherPolicy.getPolicyManager().equals(getPolicyManager())
+                && otherPolicy.getPremium().equals(getPremium());
+    }
+
+    @Override
+    public int hashCode() {
+        // use this method for custom fields hashing instead of implementing your own
+        return Objects.hash(name, company, policyManager, premium);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(getName())
+                .append("; Company: ")
+                .append(getCompany())
+                .append("; Policy Manager: ")
+                .append(getPolicyManager())
+                .append("; Premium: ")
+                .append(getPremium());
+        return builder.toString();
+    }
+
+
+}
