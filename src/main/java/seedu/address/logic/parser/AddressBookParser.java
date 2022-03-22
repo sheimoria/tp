1c11de01a63317555e3d16222ed04210f24f343b
Zechary <zechary@datature-io>
2022-03-22 22:23:09 +0800

    Remove GetPreferenceCommand (redundant feature)

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -15,7 +15,6 @@ import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
 import seedu.address.logic.commands.FindCommand;
-import seedu.address.logic.commands.GetPreferenceCommand;
 import seedu.address.logic.commands.HelpCommand;
 import seedu.address.logic.commands.ListCommand;
 import seedu.address.logic.commands.TutorialCommand;
@@ -113,9 +112,6 @@ public class AddressBookParser {
 
         case AddPreferenceCommand.COMMAND_WORD:
             return new AddPreferenceCommandParser().parse(arguments);
-
-        case GetPreferenceCommand.COMMAND_WORD:
-            return new GetPreferenceCommandParser().parse(arguments);
 
         case DeletePolicyCommand.COMMAND_WORD:
             return new DeletePolicyCommandParser().parse(arguments);
