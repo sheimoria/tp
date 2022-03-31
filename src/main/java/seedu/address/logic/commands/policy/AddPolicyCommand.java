444897b937320c3a1b411919c30c31ff01b7afb1
joelwongjy <joelwongjy@gmail.com>
2022-04-01 00:58:41 +0800

    Fix policies not updating on edit

--- src/main/java/seedu/address/logic/commands/policy/AddPolicyCommand.java
+++ src/main/java/seedu/address/logic/commands/policy/AddPolicyCommand.java
@@ -60,11 +60,11 @@ public class AddPolicyCommand extends Command {
         }
 
         Client clientToAddPolicy = lastShownList.get(index.getZeroBased());
-
-        model.setClient(clientToAddPolicy, clientToAddPolicy.addPolicy(policyToAdd));
+        Client updatedClient = clientToAddPolicy.addPolicy(policyToAdd);
+        model.setClient(clientToAddPolicy, updatedClient);
 
         return new CommandResult(String.format(MESSAGE_SUCCESS, policyToAdd), false, false, false,
-                false, false, null, clientToAddPolicy);
+                false, false, null, updatedClient);
     }
 
     @Override
