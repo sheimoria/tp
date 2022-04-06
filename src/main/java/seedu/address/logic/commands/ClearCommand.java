e6c61ddb9e0f4c68d7b55c584497016447c7a184
myc37 <nvjn37@gmail.com>
2022-04-06 17:14:21 +0800

    fix checkstyle errors

--- src/main/java/seedu/address/logic/commands/ClearCommand.java
+++ src/main/java/seedu/address/logic/commands/ClearCommand.java
@@ -2,13 +2,13 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
 import seedu.address.model.client.Client;
-
-import java.util.List;
 
 /**
  * Clears the address book.
