b979df270defc544b8d56acbfc3b2e121d883fd4
Zechary <zechary@datature-io>
2022-04-10 01:10:57 +0800

    Add more tests for AddPreferenceCommand

--- src/test/java/seedu/address/logic/commands/AddPreferenceCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPreferenceCommandTest.java
@@ -1,5 +1,7 @@
 package seedu.address.logic.commands;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PREFERENCE_CATEGORY;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_PREFERENCE_VALUE;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
@@ -7,6 +9,8 @@ import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalClients.getTypicalAddressBook;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_INVALID_CLIENT;
+import static seedu.address.testutil.TypicalIndexes.INDEX_SECOND_CLIENT;
 
 import org.junit.jupiter.api.Test;
 
@@ -39,6 +43,18 @@ public class AddPreferenceCommandTest {
     }
 
     @Test
+    public void execute_invalidClientIndex_failure() {
+        String categoryToAdd = VALID_PREFERENCE_CATEGORY;
+        String preferenceToAdd = VALID_PREFERENCE_VALUE;
+        AddPreferenceCommand addPreferenceCommand = new AddPreferenceCommand(INDEX_INVALID_CLIENT, categoryToAdd,
+                preferenceToAdd);
+        String expectedMessage = Messages.MESSAGE_INVALID_CLIENT_DISPLAYED_INDEX;
+        Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()), new UserPrefs());
+
+        assertCommandFailure(addPreferenceCommand, expectedModel, expectedMessage);
+    }
+
+    @Test
     public void execute_preferenceAcceptedByModel_success() {
         String categoryToAdd = VALID_PREFERENCE_CATEGORY;
         String preferenceToAdd = VALID_PREFERENCE_VALUE;
@@ -54,12 +70,32 @@ public class AddPreferenceCommandTest {
     @Test
     public void execute_emptyAddressBook_failure() {
         Model model = new ModelManager();
-        String categoryToAdd = VALID_PREFERENCE_CATEGORY;
-        String preferenceToAdd = VALID_PREFERENCE_VALUE;
         AddPreferenceCommand addPreferenceCommand = new AddPreferenceCommand(INDEX_FIRST_CLIENT,
                 VALID_PREFERENCE_CATEGORY, VALID_PREFERENCE_VALUE);
         String expectedMessage = String.format(Messages.MESSAGE_EMPTY_CLIENT_LIST, "add preference to");
         assertCommandFailure(addPreferenceCommand, model, expectedMessage);
+    }
+
+    @Test
+    public void equals() {
+        // same values -> returns true
+        AddPreferenceCommand addPreferenceCommandToCopy = new AddPreferenceCommand(INDEX_FIRST_CLIENT,
+                VALID_PREFERENCE_CATEGORY, VALID_PREFERENCE_VALUE);
+        assertTrue(new AddPreferenceCommand(INDEX_FIRST_CLIENT, VALID_PREFERENCE_CATEGORY, VALID_PREFERENCE_VALUE)
+                .equals(addPreferenceCommandToCopy));
+
+        // same object -> returns true;
+        assertTrue(addPreferenceCommandToCopy.equals(addPreferenceCommandToCopy));
+
+        // null -> returns false
+        assertFalse(addPreferenceCommandToCopy.equals(null));
+
+        // different type -> returns false
+        assertFalse(addPreferenceCommandToCopy.equals(5));
+
+        // different add preference command -> returns false
+        assertFalse(addPreferenceCommandToCopy.equals(new AddPreferenceCommand(INDEX_SECOND_CLIENT,
+                VALID_PREFERENCE_CATEGORY, VALID_PREFERENCE_VALUE)));
     }
 
 }
