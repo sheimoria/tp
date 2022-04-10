564ae71f415673dea9aa3e866b12bf7690ca8c39
Zechary <zechary@datature-io>
2022-04-10 16:57:59 +0800

    fix checkstyle

--- src/test/java/seedu/address/logic/parser/ViewClientCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/ViewClientCommandParserTest.java
@@ -1,18 +1,14 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.commands.CommandTestUtil.PREFERENCE_CAT_PREF;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
-import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_CLIENT;
 
 import org.junit.jupiter.api.Test;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.commands.ViewClientCommand;
-
-import javax.swing.text.View;
 
 public class ViewClientCommandParserTest {
 
